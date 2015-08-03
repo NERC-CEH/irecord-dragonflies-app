7f03a98c52a45c03878972e429b3c5292ceb881b
kazlauskis <karolis@kazlauskis.com>
2015-08-03 10:41:42 +0100

    Fixed occurrences empty list.

--- src/scripts/views/record_multi_occurrences_list.js
+++ src/scripts/views/record_multi_occurrences_list.js
@@ -2,79 +2,80 @@
  * List view of the Multi Record Species List used in ListPage view.
  *****************************************************************************/
 define([
-  'backbone',
-  'views/record_multi_occurrences_list_item',
-  'templates'
+    'backbone',
+    'views/record_multi_occurrences_list_item',
+    'templates'
 ], function (Backbone, RecordMultiOccurrencesListItemView) {
-  'use strict';
+    'use strict';
 
-  var View = Backbone.View.extend({
-    tagName: 'ul',
+    var View = Backbone.View.extend({
+        tagName: 'ul',
 
-    attributes: {
-      'data-role': 'listview'
-    },
+        attributes: {
+            'data-role': 'listview'
+        },
 
-    /**
-     * Initializes the species list view.
-     */
-    initialize: function () {
-      _log('views.RecordMultiOccurrencesList: initialize', log.DEBUG);
+        /**
+         * Initializes the species list view.
+         */
+        initialize: function () {
+            _log('views.RecordMultiOccurrencesList: initialize', log.DEBUG);
 
-      this.collection.on('change', this.update, this);
-      this.collection.on('update', this.update, this);
-    },
+            this.collection.on('change', this.update, this);
+            this.collection.on('update', this.update, this);
+        },
 
-    /**
-     * Renders the species list.
-     * @returns {SpeciesListView}
-     */
-    render: function () {
-      _log('views.RecordMultiOccurrencesList: render ', log.DEBUG);
+        /**
+         * Renders the species list.
+         * @returns {SpeciesListView}
+         */
+        render: function () {
+            _log('views.RecordMultiOccurrencesList: render ', log.DEBUG);
 
-      var container = document.createDocumentFragment(); //optimising the performance
+            var container = document.createDocumentFragment(); //optimising the performance
 
-      if (this.collection.length) {
-        _.each(this.collection.occurrences, function (occurrence) {
-          var item = new RecordMultiOccurrencesListItemView({
-              model: occurrence
-          });
-          container.insertBefore(item.render().el, container.firstChild);
-        });
+            if (this.collection.length) {
+                _.each(this.collection.occurrences, function (occurrence) {
+                    var item = new RecordMultiOccurrencesListItemView({
+                        model: occurrence
+                    });
+                    container.insertBefore(item.render().el, container.firstChild);
+                });
 
-        this.$el.html(container); //appends to DOM only once
-        this.$el.listview().listview('refresh');
+                this.$el.html(container); //appends to DOM only once
+                this.$el.listview().listview('refresh');
 
-        //attach listeners
-        $('.record-multi-occurrences-remove').on('click', function () {
-          _log('views.RecordMultiOccurrencesList: removing saved occurrence.', log.DEBUG);
+                //attach listeners
+                $('.record-multi-occurrences-remove').on('click', function () {
+                    _log('views.RecordMultiOccurrencesList: removing saved occurrence.', log.DEBUG);
 
-          var id = $(this).data('id');
-          app.models.sampleMulti.occurrences.remove(id);
-        });
-        $('#empty-list-message').hide();
-      } else {
-        $('#empty-list-message').show();
-     }
+                    var id = $(this).data('id');
+                    app.models.sampleMulti.occurrences.remove(id);
+                });
+                $('#empty-list-message').hide();
+            } else {
+                this.$el.empty();
+                $('#empty-list-message').show();
+            }
 
-      return this;
-    },
+            return this;
+        },
 
-    update: function () {
-      _log('views.RecordMultiOccurrencesList: updating', log.DEBUG);
+        update: function () {
+            _log('views.RecordMultiOccurrencesList: updating', log.DEBUG);
 
-      this.render();
+            this.render();
 
-      //attach event listeners
-      this.$el.find('.camera-picker').on('click', function (e) {
-        //disable jqm link
-        e.stopPropagation();
-        e.preventDefault();
+            //attach event listeners
+            this.$el.find('.camera-picker').on('click', function (e) {
+                //disable jqm link
+                e.stopPropagation();
+                e.preventDefault();
 
-        app.message('<center><b>Photo picker disabled</b></center>', 500);
-      });
-    }
-  });
+                app.message('<center><b>Photo picker disabled</b></center>', 500);
+            });
+        }
+    });
 
-  return View;
+    return View;
 });
