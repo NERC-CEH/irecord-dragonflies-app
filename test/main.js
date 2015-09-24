55faf1002ebf57ac8745fd11b815b51a35b86283
kazlauskis <karolis@kazlauskis.com>
2015-09-24 17:08:07 +0100

    Added basic app testing.

--- test/main.js
+++ test/main.js
@@ -5,28 +5,162 @@
 define(['morel'], function (morel) {
     window.app || (window.app = {});
 
-    var test = {
-        run: function () {
-            this.populateDBSamples(45);
+    var db = {
+        init: false,
+
+        initialize: function () {
+            if (!this.init) {
+                this.recordManager = new morel.Manager(app.CONF.morel);
+                this.init = true;
+            }
         },
 
-        clean: function () {
-            this.recordManager.clear();
-        },
+        /**
+         * TESTING: storage capabilities
+         *
+         * @param samplesSize in Mb
+         * @param samplesNumber will be Kb each
+         */
+        run: function (samplesSize, samplesNumber) {
+            var that = this;
+            samplesSize || (samplesSize = 49); //49Mb
+            samplesNumber || (samplesNumber = 1000); //number of records
 
-        populateDBSamples: function (number) {
-            this.recordManager = new morel.Manager({
-                Storage: morel.DatabaseStorage
+            //lets make it clean
+            this.clear(function (err) {
+                if (err) { printError(err); return;}
+
+                //TEST: populate DB by size
+                that.add(samplesSize, null, function (err, data) {
+                    if (err) { printError(err); return;}
+
+                    that.size(null, function (err, size) {
+                        if (err) { printError(err); return;}
+
+                        size = size / 1024;
+                        var bias = samplesSize * 21; //happens to be like this on Chrome
+
+                        //should be samplesNumber x1024Kb -+biasKb
+                        if (size > (samplesSize * 1000 - bias) &&
+                            size < (samplesSize * 1000 + bias)) {
+                            console.log('Size: OK (' +
+                                samplesSize + ' - ' + (size).toFixed(2) +
+                                'Kb +-' + bias +')');
+                        } else {
+                            console.error('Size: FAILED (' +
+                                samplesSize + ' - ' + (size).toFixed(2) +
+                                'Kb +-' + bias +')');
+                        }
+
+                        //TEST: clean up
+                        that.clear(function (err) {
+                            if (err) { printError(err); return;}
+
+                            that.size(null, function (err, size) {
+                                if (err) { printError(err); return;}
+
+                                size = size / 1024;
+                                if (size < 10) {
+                                    console.log('Clear: OK (' + (size).toFixed(2) + 'Kb)');
+                                } else {
+                                    console.error('Clear: FAILED (' + (size).toFixed(2) + 'Kb)');
+                                }
+
+                                //TEST: populate DB by number
+                                that.add(null, samplesNumber, function (err, data) {
+                                    if (err) { printError(err); return;}
+
+                                    that.size(true, function (err, size) {
+                                        if (err) { printError(err); return;}
+
+                                        //should be samplesNumber x1000Kb -+biasKb
+                                        if (size === samplesNumber) {
+                                            console.log('Numbers: OK (' + size + ')');
+                                        } else {
+                                            console.error('Numbers: FAILED (' + samplesNumber + ' - ' + size + ')');
+                                        }
+
+                                        //TEST: clean up
+                                        that.clear(function (err) {
+                                            if (err) { printError(err); return;}
+
+                                            that.size(null, function (err, size) {
+                                                if (err) { printError(err); return;}
+
+                                                size = size / 1024;
+                                                if (size < 10) {
+                                                    console.log('Clear: OK (' + (size).toFixed(2) + 'Kb)');
+                                                } else {
+                                                    console.error('Clear: FAILED (' + (size).toFixed(2) + 'Kb)');
+                                                }
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
             });
-            var sample = null,
-                megabyte = this._getMegabyte();
+        },
 
-            //1000 records
+        clear: function (callback) {
+            this.init || this.initialize();
+            this.recordManager.clear(callback);
+        },
+
+        /**
+         * Add by size or number of records
+         * @param size in Mb
+         * @param number of records
+         * @param callback
+         */
+        add: function (size, number, callback) {
+            this.init || this.initialize();
+
+            number || (number = size);
+
+            var added = 0,
+                sample = null,
+                data = size ? this._getMegabyte() : this._getKilobyte();
+
             for (var i = 0; i < number; i++) {
                 sample = new morel.Sample({
-                    attributes: {data: megabyte}
+                    attributes: {
+                        date: morel.formatDate(randomDate(new Date(2012, 0, 1), new Date())),
+                        data: data
+                    }
                 });
-                this.recordManager.set(sample)
+                sample.occurrences.add(new morel.Occurrence({
+                    attributes: {
+                        taxon: 1
+                    }
+                }));
+                this.recordManager.set(sample, function (err, data) {
+                    added++;
+
+                    if (added === number) {
+                        callback(err, data);
+                    }
+                })
+            }
+
+            function randomDate(start, end) {
+                return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+            }
+
+        },
+
+        size: function (count, callback) {
+            this.init || this.initialize();
+            if (count) {
+                this.recordManager.storage.getAll(function(err, data){
+                    callback (err, data.length);
+                });
+            } else {
+                this.recordManager.storage.size(function(err, size){
+                    callback (err, size);
+                });
             }
         },
 
@@ -42,8 +176,10 @@ define(['morel'], function (morel) {
         _getKilobyte: function () {
             var data = '';
 
-            for (var i = 0; i < 1000; i++) {
-                data += '1';
+            for (var i = 0; i < 500; i++) {
+                var rand =  (Math.random() * 100).toFixed(0);
+                rand = rand >= 10 ? rand : (rand + '1');
+                data += rand;
             }
             return data;
         }
@@ -51,5 +187,10 @@ define(['morel'], function (morel) {
 
     };
 
-    app.test = test;
+    var err = function (err) {
+            console.error('FAILED (' + err.message + ')');
+    };
+
+    app.test = {};
+    app.test.db = db;
 });
