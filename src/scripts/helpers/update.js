a85435d54bdf5d425fe05d3109a7bf6b13e889f1
kazlauskis <karolis@kazlauskis.com>
2015-10-07 17:59:37 +0100

    Bumped to 1.1.0

--- src/scripts/helpers/update.js
+++ src/scripts/helpers/update.js
@@ -3,21 +3,42 @@
  * data's version.
  *****************************************************************************/
 define([], function () {
-  var CheckForUpdates = function () {
-    var appVer = app.models.app.get('appVer');
-    if (appVer !== app.VERSION) {
-      _log('helpers: app version differs. Updating.', log.INFO);
+    var CheckForUpdates = function () {
+        var appVer = app.models.app.get('appVer');
+        if (appVer !== app.VERSION) {
+            _log('helpers: app version differs. Updating.', log.INFO);
 
-      //set new version
-      app.models.app.save('appVer', app.VERSION);
+            switch (appVer) {
+                case '1.0.0':
+                case '1.0.1':
+                case '1.0.2':
+                    var location = app.models.user.get('location');
+                    if (location){
+                        var loc = location.split(',');
+                        if (loc.length) {
+                            app.models.user.setLocation({
+                                latitude: loc[0],
+                                longitude: loc[1],
+                                accurracy: 1,
+                                name: ''
+                            })
+                        }
+                    }
 
-      if (app.CONF.GA.STATUS) {
-        require(['ga'], function(ga) {
-          ga('send', 'event', 'app', 'updateSuccess');
-        });
-      }
-    }
-  };
+                    break;
+                default:
 
-  return CheckForUpdates;
+            }
+            //set new version
+            app.models.app.save('appVer', app.VERSION);
+
+            if (app.CONF.GA.STATUS) {
+                require(['ga'], function(ga) {
+                    ga('send', 'event', 'app', 'updateSuccess');
+                });
+            }
+        }
+    };
+
+    return CheckForUpdates;
 });
